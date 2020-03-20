06dad1e3d0092f60c3b727833bfbf0e9827cd3eb
uzairr <uzairr@yahoo.com>
2020-03-20 17:49:01 +0500

    Update bulk un-enroll management command
    PROD-1370

--- common/djangoapps/student/management/commands/bulk_unenroll.py
+++ common/djangoapps/student/management/commands/bulk_unenroll.py
@@ -27,18 +27,29 @@ class Command(BaseCommand):
                             dest='csv_path',
                             required=False,
                             help='Path to CSV file.')
+        parser.add_argument('-c', '--course-id',
+                            metavar='course_id',
+                            dest='course-id',
+                            required=False,
+                            help='unenroll all users from provided course-id')
 
     def handle(self, *args, **options):
 
         csv_path = options['csv_path']
+        course_id = options['course-id']
+
+        if course_id:
+            self.unenroll_all_users(course_id=course_id)
+            return
+
         if csv_path:
             with open(csv_path, 'rb') as csv_file:
-                self.unenroll_users(csv_file)
+                self.unenroll_users_from_csv(csv_file)
         else:
             csv_file = BulkUnenrollConfiguration.current().csv_file
-            self.unenroll_users(csv_file)
+            self.unenroll_users_from_csv(csv_file)
 
-    def unenroll_users(self, csv_file):
+    def unenroll_users_from_csv(self, csv_file):
         reader = list(unicodecsv.DictReader(csv_file))
         users_unenrolled = {}
         for row in reader:
@@ -70,3 +81,22 @@ class Command(BaseCommand):
 
         logger.info("Following users have been unenrolled successfully from the following courses: {users_unenrolled}"
                     .format(users_unenrolled=["{}:{}".format(k, v) for k, v in users_unenrolled.items()]))
+
+    def unenroll_all_users(self, course_id):
+        """
+        Un-enroll all users from a given course
+        """
+        try:
+            course_id = CourseKey.from_string(course_id)
+        except InvalidKeyError:
+            msg = 'Invalid course id {}, skipping un-enrollement.'.format(course_id)
+            logger.warning(msg)
+            return
+
+        try:
+            updated_count = CourseEnrollment.objects.filter(course_id=course_id).update(is_active=False)
+            logger.info("{} users have been unenrolled successfully from the provided course: {}"
+                        .format(updated_count, course_id))
+        except Exception as err:
+            msg = 'Error un-enrolling Users from course {}: with error: {}'.format(course_id, err)
+            logger.error(msg, exc_info=True)
